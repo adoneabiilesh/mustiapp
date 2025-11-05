2f31e98c59ab39b07c7403b095a4828ae8d1590b
adoneabiilesh <hitimu9@gmail.com>
2025-11-05 02:17:39 +0100

    Remove react-native-worklets, use react-native-reanimated instead

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,16 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+            ["babel-preset-expo", { 
+                jsxImportSource: "nativewind",
+                // Disable react-native-worklets plugin since it's not compatible with RN 0.76.9
+                // react-native-reanimated provides worklets support instead
+            }],
             "nativewind/babel",
+        ],
+        plugins: [
+            // react-native-reanimated plugin must be listed last
+            "react-native-reanimated/plugin",
         ],
     };
 };
